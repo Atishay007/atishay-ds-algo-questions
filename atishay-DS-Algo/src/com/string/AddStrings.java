4597e4c47f35cd23f95adc51c60a2987ff8426fd
Atishay Jain <atishayjain23@gmail.com>
2023-02-10 19:15:47 +0530

    Added Leetcode URL

--- atishay-DS-Algo/src/com/string/AddStrings.java
+++ atishay-DS-Algo/src/com/string/AddStrings.java
@@ -1,13 +1,36 @@
 package com.string;
 
+/**
+ * https://leetcode.com/problems/add-strings/
+ * TC:O(n+m) length of both Strings
+ * SC:O(1): Didn't used any extra space, modifications happening in the String which is > in length
+ * <p>
+ * Can use used in: https://leetcode.com/problems/multiply-strings/description/
+ */
 public class AddStrings {
     public static void main(String[] args) {
-        String num1 = "888888";
-        String num2 = "99009009900";
-        System.out.println(addStrings(num1, num2));
+        String num1 = "2233569940800578652115";
+        String num2 = "238255300016431168468";
+
+        String[] strings = {"1496485980588", "14964859805880", "249414330098000", "4489457941764000", "9976573203920000", "299297196117600000", "3491800621372000000", "34918006213720000000", "199531464078400000000", "1995314640784147483647", " 3990629281568147483647"};
+
+        String result = null;
+        for (int i = 0; i < strings.length; i++) {
+            result = addStrings(strings[i], result);
+            System.out.println("Result: " + i + " " + result);
+        }
+
+
+        // System.out.println(addStrings(num1, num2));
     }
 
-    private static String addStrings(String s1, String s2) {
+    public static String addStrings(String s1, String s2) {
+        if (s1 == null) {
+            return s2;
+        } else if (s2 == null) {
+            return s1;
+        }
+
         if (s2.length() > s1.length()) {
             String tmp = s1;
             s1 = s2;
@@ -15,8 +38,8 @@ public class AddStrings {
         }
 
         StringBuilder builder1 = new StringBuilder(s1);
-        builder1.reverse();
         StringBuilder builder2 = new StringBuilder(s2);
+        builder1.reverse();
         builder2.reverse();
 
         int carryF = 0;
@@ -57,4 +80,3 @@ public class AddStrings {
         return carryF;
     }
 }
-
